f28c64339a12cba65dfa85c98d858cc77f3c5b95
Jan Kulisek <jkulisek.us@gmail.com>
2022-12-03 21:35:31 -0800

    Fixed warning msg after login

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -6,49 +6,49 @@ class Home_model extends CI_Model {
 		$this->load->database();
 		date_default_timezone_set("America/Los_Angeles");
 	}
-	
+
 	public function send_msg($param) {
-		
-		$recipient = 'jank@jlkconsulting.info';
-		$message = "Name: " . $param['name'] . "\n\n" . "Email: " .  $param['email'] . "\n\n" . "Message:\n\n" . $param['msg'];
-		
+		$msg = 'Bogus!';
 		//Send mail
-		if(mail($recipient, 'From kulisek.org: ' . $param['subj'], $message)) {
-			$msg = 'Your message has been sent. Thank you.';
+		if (filter_var($param['email'], FILTER_VALIDATE_EMAIL)) {
+			$recipient = 'jank@jlkconsulting.info';
+			$message = "Name: " . $param['name'] . "\n\n" . "Email: " .  $param['email'] . "\n\n" . "Message:\n\n" . $param['msg'];
+			if(mail($recipient, 'From kulisek.org: ' . $param['subj'], $message)) {
+				$msg = 'Your message has been sent. Thank you.';
+			}
+			else {
+				$msg = 'There has been an error while sending your message. Please, try again. Thank you';
+			}
+
+			$this->db->insert('messages', $param);
 		}
-		else {
-			$msg = 'There has been an error while sending your message. Please, try again. Thank you'; 
-		}
-		
-		$this->db->insert('messages', $param);
-		
 		return $msg;
 	}
-	
+
 	public function get_blog() {
-		
+
 		$retarr = array();
-		
+
 		if (session_status() !== PHP_SESSION_ACTIVE) {
 			session_start();
 			session_regenerate_id(FALSE);
 		}
-		
+
 		$this->db->select('fname, lname');
 		$this->db->where('level', 99);
 		$q = $this->db->get('users')->row();
-		
+
 		$retarr['user']['fname'] = $q->fname;
 		$retarr['user']['lname'] = $q->lname;
-		
+
 		$entries = $this->Blog_model->get_partial_entries(10, 0);
-		
+
 		$retarr['exerpts'] = $entries['entries'];
 		$retarr['pinned'] = $entries['pinned'];
-		
+
 		$retarr['subjects'] = $this->arr_lib->subjects();
-		
+
 		return $retarr;
 	}
-	
+
 }
--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -5,94 +5,95 @@ class Login_model extends CI_Model {
 		parent::__construct();
 		$this->load->database();
 	}
-	
+
 	function is_logged() {
 		$retval = FALSE;
-		
+
 		if (session_status() !== PHP_SESSION_ACTIVE) {
 			session_start();
 		}
-		
+
 		if(isset($_SESSION['logged'])) {
 			$retval = $_SESSION['logged'];
 			$retval = TRUE;
 		}
-		
+
 		return $retval;
 	}
-	
+
 	public function validate_user($param) {
-		$retval = FALSE;		
+		$retval = FALSE;
 		if(!$this->is_logged()) {
 			$this->db->select('username, password');
 			$this->db->where('username', $param['user']);
-			$user_data = $this->db->get('users')->row();
+
 			//echo '<br><br>user: ' . $param['user'];
-			if(count($user_data) == 1) {
+			if($this->db->count_all_results('users')) {
+				$user_data = $this->db->get('users')->row();
 				if(password_verify($param['pass'], $user_data->password)) {
 					$retval = TRUE;
-					
+
 					if (session_status() !== PHP_SESSION_ACTIVE) {
 						session_start();
 						session_regenerate_id(FALSE);
 					}
-					$date = time();	
-					
+					$date = time();
+
 					$_SESSION['logged'] = TRUE;
 					$_SESSION['date'] = $date;
 					$_SESSION['user']['username'] = $param['user'];
-					
+
 					$this->db->select('*');
 					$this->db->where('username', $param['user']);
-					
+
 					$user = $this->db->get('users')->row();
-					
+
 					$_SESSION['user']['id_user'] = $user->id_users;
 					$_SESSION['user']['fname'] = $user->fname;
 					$_SESSION['user']['lname'] = $user->lname;
-					
+
 					$this->db->where('id', session_id());
 					$this->db->update('sessions', array('logged' => 1, 'date' => $date, 'id_user' => $_SESSION['user']['id_user']));
 				}
-			}	
+			}
 		}
 		else {
 			$retval = TRUE;
 		}
 		return $retval;
 	}
-	
+
 	public function check_login() {
 		$retval = TRUE;
-		
+
 		$this->db->select_max('id_sessions');
 		$max = $this->db->get('sessions')->row()->id_sessions;
-		
+
 		$this->db->select('logged');
 		$this->db->where('id_sessions', $max);
-		
+
 		if($this->db->get('sessions')->row()->logged == 1) {
 			$retval = TRUE;
 		}
-		
+
 		return $retval;
 	}
-	
+
 	public function logout() {
 		if (session_status() !== PHP_SESSION_ACTIVE) {
 			session_start();
 		}
-		
+
 		$this->db->where('id', session_id());
 		$this->db->update('sessions', array('logged' => 0));
-		
+
 		//if(isset($_SESSION['logged'])) {
 			unset($_SESSION['logged']);
 			unset($_SESSION['date']);
 		//}
-		
+
 		session_regenerate_id(FALSE);
 		session_destroy();
 	}
-	
+
 }
